541780343823a5451f80f2c0752a6f4b07831c1a
tgrabar <ted.grabar@gmail.com>
2023-10-03 00:11:51 -0700

    Add mouseover event to change cell color

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 const gridWidth = 16;
 const container = document.querySelector('.container');
 
+// create grid with height = width
 for (let i = 1; i <= Math.pow(gridWidth, 2); i++) {
   const gridCell = document.createElement('div');
   gridCell.classList.add('grid-cell');
   container.appendChild(gridCell);
+}
+
+document.querySelectorAll('.grid-cell').forEach(cell => {
+  cell.addEventListener('mouseenter', () => changeColor(cell))
+})
+
+function changeColor(cell) {
+  cell.style.backgroundColor = 'black';
 }
